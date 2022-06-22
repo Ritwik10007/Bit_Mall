d83761f024260f13c70330b5a07eedf141272e2b
Ritwik10007 <97779823+Ritwik10007@users.noreply.github.com>
2022-06-23 05:04:02 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Ritwik";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalog App")),
+      body: Center(
+        child: Container(
+          child: Text("My flutter app $days by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
